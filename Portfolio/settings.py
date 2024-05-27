a8c82dbbaf7fcc301058ad3b49d610fa2d1859a8
Nawaj Sarif <94285331+Nawaj2417@users.noreply.github.com>
2024-05-27 12:03:27 +0000

    changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7vojccz6!q8d6p68smdsrkv^d5^pb@uldzueq1yg5lya_ro+69
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh","127.0.0.1", "*"]
 
 
 # Application definition
