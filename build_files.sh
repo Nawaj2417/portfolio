2b39b6b926cf79a483d8c4fb7ee617f66adae46a
Nawaj2417 <mdnawajn27@gmail.com>
2024-05-27 21:53:19 +0545

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # echo "BUILD_START"
 pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic 
 sudo apt install python3-pip
 pip install python
 # echo "BUILD END"
