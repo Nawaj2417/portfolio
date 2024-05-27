3fad2575bc80f95e0a79d61c501eb31d684f51e0
Nawaj Sarif <94285331+Nawaj2417@users.noreply.github.com>
2024-05-27 12:02:23 +0000

    new changes

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-echo "BUILD_START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+# echo "BUILD_START"
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
