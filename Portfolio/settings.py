501a27e4d25c45a51d5bda444aea400a9c1ea31a
Nawaj Sarif <94285331+Nawaj2417@users.noreply.github.com>
2024-05-29 09:41:53 +0545

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # vercel statics settings
-import os
+
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static")
